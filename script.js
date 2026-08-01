2b4da3a1d76943c14469eeea9d87c42ac9f8b6e1
Aiden-Sharples <aiden.sharples@dcmail.ca>
2026-07-31 20:22:06 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+// Image data
+const images = [
+  { filename: "pic1.jpg", alt: "Closeup of a human eye" },
+  { filename: "pic2.jpg", alt: "Rock that looks like a wave" },
+  { filename: "pic3.jpg", alt: "Purple and white pansies" },
+  { filename: "pic4.jpg", alt: "Section of wall from a pharaoh's tomb" },
+  { filename: "pic5.jpg", alt: "Large moth on a leaf" }
+];
+
+// Add thumbnails
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (const image of images) {
+  const newImage = document.createElement("img");
+  newImage.src = baseURL + image.filename;
+  newImage.alt = image.alt;
+  newImage.setAttribute("tabindex", "0");
+  thumbBar.appendChild(newImage);
+
+  newImage.addEventListener("click", updateDisplayedImage);
+  newImage.addEventListener("keydown", (e) => {
+    if (e.code === "Enter") updateDisplayedImage.call(newImage);
+  });
+
+
+}
+
+// Update displayed image
+function updateDisplayedImage(e) {
+  displayedImage.src = e.target.src;
+  displayedImage.alt = e.target.alt;
+}
+
+// Add event listener button
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+
+  btn.classList.toggle("dark");
+});
